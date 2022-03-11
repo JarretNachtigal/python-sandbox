4c43cac5f1426ca76a9eb0d3ed37460ba68836ab
Jarret Nachtigal <jarretn1224@gmail.com>
2022-03-11 16:48:18 -0500

    sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,12 @@
-arr = [1, 2, 3, 4, 5]
+import string
+# arr = [1, 2, 3, 4, 5]
 
-print(arr[:4])  # from beginning to index 4 noninclusive
-print(arr[1:])  # from index 1 inclusive to end
-print(arr[::4])  # index 0, index 4
-print(arr[1:][::3])  # index 1, step by 3
-print(arr[1:][::-1])  # index 1 and on, reversed
-print(arr[::-1])  # reverse
+# print(arr[:4])  # from beginning to index 4 noninclusive
+# print(arr[1:])  # from index 1 inclusive to end
+# print(arr[::4])  # index 0, index 4
+# print(arr[1:][::3])  # index 1, step by 3
+# print(arr[1:][::-1])  # index 1 and on, reversed
+# print(arr[::-1])  # reverse
+
+arr = list(string.ascii_lowercase)
+print(arr)
