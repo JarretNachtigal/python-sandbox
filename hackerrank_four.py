7ff3939387a6116c591b66c69f20b60ea8b6e3bf
Jarret Nachtigal <jarretn1224@gmail.com>
2022-03-16 15:42:21 -0400

    hackerrank

--- hackerrank_four.py
+++ hackerrank_four.py
@@ -1,3 +1,6 @@
+import math
+
+
 def saveThePrisoner(n, m, s):
     # Write your code here
     # n - number of prisoners
@@ -5,6 +8,8 @@ def saveThePrisoner(n, m, s):
     # s - starting chair number (not an index)
     i = m
     count = s
+    while m > n:
+        m -= n
     while i > 1:
         if count < n:
             count += 1
@@ -16,22 +21,21 @@ def saveThePrisoner(n, m, s):
 
 
 def saveThePrisonerTwo(n, m, s):
-    # Write your code here
     # n - number of prisoners
     # m - number of sweets
     # s - starting chair number (not an index)
-    count = s
-    remaining_sweets = m
-    while remaining_sweets > n:
-        remaining_sweets -= n
-    count += remaining_sweets
-    if count > n:
-        count -= n
-    return count
+    # return the seat number when m - 1 sweets have been given
+    remainder = m % n
+    # end on last chair
+    if (remainder + s - 1) % n == 0:
+        return n
+    else:
+        return (remainder + s - 1) % n
 
 
-print(7 % 19)
-print(19 % 7)  # remainder from 19 / 7 = 2 (+ 5)
-
-
+    # print(7 % 19)
+    # print(19 % 7)  # remainder from 19 / 7 = 2 (+ 5)
 print(saveThePrisonerTwo(7, 19, 2))  # => 6
+print(saveThePrisonerTwo(5, 2, 1))  # => 2
+print(saveThePrisonerTwo(5, 2, 2))  # => 3
+print(saveThePrisonerTwo(352926151, 380324688, 94730870))  # => 122129406
