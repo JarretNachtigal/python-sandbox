dcd2afc5fde94f1e3b2e1b10d4c52affddb9c644
Jarret Nachtigal <jarretn1224@gmail.com>
2022-04-04 15:08:30 -0400

    hackerrank question

---
+++ hackerrank_4_4_2022.py
@@ -0,0 +1,33 @@
+# N = int(input())
+# arr = []
+# i = 0
+# while i < N:
+#      command = input()  # read line
+#       command = command.split(" ")
+#        # print("command is ", command)
+#        # print("array is", arr)
+#        if command[0] == "insert":
+#             index = int(command[1])
+#             num = int(command[2])
+#             # print("inserting", num, "at index", index)
+#             arr.insert(index, num)
+#         elif command[0] == "print":
+#             print(arr)
+#         elif command[0] == "remove":
+#             # print("removing", int(command[1]))
+#             arr.remove(int(command[1]))
+#         elif command[0] == "append":
+#             # print("appending", int(command[1]))
+#             arr.append(int(command[1]))
+#         elif command[0] == "sort":
+#             # print("sorting")
+#             arr.sort()
+#         elif command[0] == "pop":
+#             # print("popping")
+#             arr.pop()
+#         elif command[0] == "reverse":
+#             # print("reversing")
+#             arr.reverse()
+#         i += 1
+
+# spacing is messed up
