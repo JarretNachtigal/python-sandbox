3f0fdc4b931c6a6be17127d95444e19620034345
Jarret Nachtigal <jarretn1224@gmail.com>
2022-04-12 15:05:00 -0400

    second problem

--- hackerrank_4_12_2022.py
+++ hackerrank_4_12_2022.py
@@ -1,5 +1,5 @@
 
-# 1 -  Python/Basic Data Types/Tuples
+# 1 - Python/Basic Data Types/Tuples
 
 # # given n number of integers
 # n = int(input())
@@ -8,3 +8,48 @@
 # integer_tuple = tuple(integer_list) #go from map object to tuple
 # # compute and print result of hash(integer_tuple)
 # print(hash(integer_tuple))
+
+# --------------------------------------------------------
+
+# 2 - Python/Regex and Parsing/HTML Parser Part 1
+
+from html.parser import HTMLParser
+n = int(input())  # number of lines to parse
+
+# create a subclass and override the handler methods
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start :", tag)
+        # attrs is a list of tuples
+        length = len(attrs)
+        # loop through list
+        for i in range(length):
+            # print inner tuple
+            inner_length = len(attrs[i])
+            # loop through fields in i
+            for n in range(0, inner_length, 2):
+                print("->", attrs[i][n], ">", attrs[i][n+1])
+
+    def handle_endtag(self, tag):
+        print("End   :", tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print("Empty :", tag)
+        # attrs is a list of tuples
+        length = len(attrs)
+        # loop through list
+        for i in range(length):
+            # print inner tuple
+            inner_length = len(attrs[i])
+            # loop through fields in i
+            for n in range(0, inner_length, 2):
+                print("->", attrs[i][n], ">", attrs[i][n+1])
+
+
+# instantiate the parser and feed it some HTML
+parser = MyHTMLParser()
+for _ in range(n):
+    parser.feed(input())
+parser.close()
