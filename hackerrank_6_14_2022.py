9230ac08dd081b3ab04371a18b8338e4a2bfea7a
Jarret Nachtigal <jarretn1224@gmail.com>
2022-06-14 12:08:46 -0400

    first problem

---
+++ hackerrank_6_14_2022.py
@@ -0,0 +1,16 @@
+# 1 ------ String Split and Join ------
+
+# input = input("input a sentance ")
+
+
+# def split_and_join(str):
+#     arr = str.split(" ")
+#     str = "-".join(arr)
+#     return str
+
+
+# print(split_and_join(input))
+
+# 2 ------ What's your name" ------
+
+input = input()
