05981a755e2660db7cb4c868f47d210a691f4094
Jarret Nachtigal <jarretn1224@gmail.com>
2022-06-14 13:59:48 -0400

    progress

--- hackerrank_6_14_2022.py
+++ hackerrank_6_14_2022.py
@@ -16,28 +16,28 @@
 # input = input()
 
 
+# kevin - starts with vowels
+# stuart - starts with consonants
 def minion_game(string):
     string = string.lower()
     vowels = {"a": False, "e": False, "i": False, "o": False,
               "u": False}  # False - not used, string index - used
-    # kevin - starts with vowels
-    # stuart - starts with consonants
 
     # determine winner and return, default stuart
-    stuart_score = get_stuart_score()
+    stuart_score = get_stuart_score(string, vowels)
     winner_score = stuart_score
     winner = "Stuart"
-    kevin_score = get_kevin_score()
+    kevin_score = get_kevin_score(string, vowels)
+    # swap winner if needed
     if stuart_score < kevin_score:
         winner = "Kevin"
         winner_score = kevin_score
 
     return f"{winner} {winner_score}"
 
+
 # this method will be used to find Kevin's score
 # this method will find all substring's of a given input string that start with vowels, nonrepeating
-
-
 def get_vowel_substrings(string, vowels):
     used_vowels = []
     for index, letter in enumerate(string):
@@ -46,23 +46,20 @@ def get_vowel_substrings(string, vowels):
             vowels[letter] = index  # save index of first instace of each value
             used_vowels.append(letter)
 
+
 # this method will be used to find Stuart's score
 # this method will find all substring's of a given input string that start with consonants, nonrepeating
-
-
 def get_consonants_substrings(string, vowels, used_vowels):
     pass
 
+
 # beginning with consonant
-
-
-def get_stuart_score():
+def get_stuart_score(string):
     pass
 
+
 # beginning with vowel
-
-
-def get_kevin_score():
+def get_kevin_score(string):
     pass
 
 
